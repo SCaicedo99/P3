b8fbb7d57eb1576095a7a6f3a53b2361c198ec3f
SCaicedo99 <43104210+SCaicedo99@users.noreply.github.com>
2018-12-21 15:51:58 -0500

    Add files via upload

---
+++ DFA.py
@@ -0,0 +1,111 @@
+from ArtistConnections import Vertex, Edge
+from SongLibrary import SongLibrary
+
+class DFA:
+
+    def __init__(self, s=None):
+        self.start = s
+
+    """
+    Build the DFA graph from the figure in task 2
+
+    """
+    def build_DFA(self):
+        v0 = Vertex(0)
+        self.start = v0
+        v1 = Vertex(1)
+        v2 = Vertex(2)
+        v3 = Vertex(3)
+        v4 = Vertex(4)
+        v5 = Vertex(5)
+        v6 = Vertex(6)
+        v7 = Vertex(7)
+
+        v7.setAcceptingState()
+
+        v0.addEdge(Edge('A', v1))
+        v1.addEdge(Edge('A', v1))
+        v1.addEdge(Edge('C', v2))
+        v2.addEdge(Edge('A', v3))
+        v3.addEdge(Edge('T', v1))
+        v3.addEdge(Edge('C', v4))
+        v4.addEdge(Edge('A', v5))
+        v5.addEdge(Edge('G', v6))
+        v5.addEdge(Edge('C', v4))
+        v5.addEdge(Edge('T', v1))
+        v6.addEdge(Edge('A', v7))
+        v7.addEdge(Edge('A', v1))
+        v7.addEdge(Edge('C', v2))
+
+        return
+
+
+
+    """
+    Test whether the input sequence seq will be accepted by the state machine
+    return True if accept
+
+    """
+    def testMatch(self, seq):
+
+        characters = list(seq)
+        current = self.start
+        for i in range(len(characters)):
+            next = current.followEdge(characters[i])
+            if next is None:
+                return False
+            else:
+                current = next
+        if current.isAcceptingState:
+            return True
+        return False
+
+    """
+    Test whether the one suffix of the input sequence seq will be accepted by the state machine
+    return the index position if accept
+    return -1 if not accept
+
+    """
+    def testAccept(self, seq):
+        indx = -1
+        characters = list(seq)
+        helper = True
+        for i in range(len(characters)):
+            if self.testMatch(seq[i:]):
+                indx = i
+                helper = True
+            else:
+                helper = False
+                indx == -1
+        if helper:
+            return indx
+        else:
+            return -1
+
+
+
+    """
+    For every song in the song library array, test whether they will be accepted by the state machine
+    Store the match index or -1 into the matchIndx array.
+    Please make sure the order of songs in the songlibrary is the same as the input file
+
+    """
+    def testSongLibrary(self, song_lib):
+        matchIndx = []
+        for song in song_lib.songArray:
+            indx = self.testAccept(song.DNA)
+            matchIndx.append(indx)
+        return matchIndx
+
+
+# WRITE YOUR OWN TEST UNDER THAT IF YOU NEED
+if __name__ == '__main__':
+
+    dfa = DFA()
+    dfa.build_DFA()
+    song_lib = SongLibrary()   # Creating a new songLibrary object
+    song_lib.loadLibrary()   # Loading the library
+    # print(dfa.testMatch('AACACATCACAGA'))     # Testing a sequence
+    # print(dfa.testAccept('CGTAGTCTAGAGCACACTAAATGAGACATCTTAGAGGAGATAGGCGTAGATCCGGTTACAACACATCACAGA'))
+    # print(dfa.testSongLibrary(song_lib))  # Testing song library
+    print("finish")
